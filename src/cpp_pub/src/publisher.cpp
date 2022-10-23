a2e65c35f45b43ee009fe293d54bdb3212d14b7c
OZiad <omar.ziad1@outlook.com>
2022-10-23 00:29:36 -0400

    Initialize publisher node and publish to "topic"

---
+++ src/cpp_pub/src/publisher.cpp
@@ -0,0 +1,37 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+using namespace std::chrono_literals;
+
+class Publisher : public rclcpp::Node
+{
+public:
+    Publisher() : Node("publisher")
+    {
+        // Assign publisher object to pub_ shared pointer
+        pub_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        timer_ = this->create_wall_timer(1s, std::bind(&Publisher::callback_function, this));
+    }
+
+private:
+    void callback_function()
+    {
+        auto message = std_msgs::msg::String();
+        std::string sub_count = std::to_string(this->count_subscribers("topic"));
+        message.data = "Number of subscribers to this topic: " + sub_count;
+        RCLCPP_INFO(this->get_logger(), "publishing: '%s'", message.data.c_str());
+        pub_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Publisher>());
+    return 0;
+}
